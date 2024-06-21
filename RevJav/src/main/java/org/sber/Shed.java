8f7d4990e4ae25d5d2839e022d7fd8bdb50e7e05
ShchekoturovDA <shchekoturovdmitr@mail.ru>
2024-06-21 21:49:57 +0300

    Reapply "tsk2"
    This reverts commit c9cd7ad2f34e494af8613173c6246ddbd679f0f7.

--- RevJav/src/main/java/org/sber/Shed.java
+++ RevJav/src/main/java/org/sber/Shed.java
@@ -1,35 +1,68 @@
 package org.sber;
 
+import lombok.NonNull;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.annotation.*;
 
 @Daily(day = 6)
-public class Shed {
+abstract class Shed {
+    abstract void xExec( DataContainer dc);
+    abstract void Fin( DataContainer dc);
 }
 
 class ShedMor extends Shed{
-    public void newCom(@NotNull DataContainer rook, String name){
-        rook.setName(name);
-        rook.setPasslvl(1);
-        System.out.println(rook.getName());
+
+    @Override
+    @Timtim(hour=8, prior = 4, descr = "Newcomer reg")
+    void xExec( DataContainer dc) {
+            dc.setPasslvl(1);
+            System.out.println(dc.getName() + "got lvl 1");
+    }
+
+    @Override
+    @Timtim(hour=10, prior = 1, descr = "Newcomer went")
+    void Fin( DataContainer dc) {
+            dc.setName("Avarage Worker #953");
+            System.out.println(dc.getName() + " you are");
     }
 }
 
 class ShedEv extends Shed{
-    public void Check(@NotNull DataContainer work){
+
+    @Timtim(hour=16, prior = 3, descr = "Try and fail")
+    @Override
+    void xExec( DataContainer dc) {
         int req = 3;
-        if (work.getPasslvl() < req) {
-            System.out.println(work.getName() + " Denied");
+        if (dc.getPasslvl() < req) {
+            dc.setAge(dc.getAge() * dc.getPasslvl());
+            System.out.println(dc.getName() + " Denied");
         } else {
-            System.out.println(work.getAge());
+            dc.setAge(25);
+            System.out.println(dc.getAge());
         }
+    }
+
+    @Timtim(hour=18, prior = 2, descr = "Against all")
+    @Override
+    void Fin( DataContainer dc) {
+        dc.setAdress("Lost hwy 65");
+        dc.setName("Last Call");
     }
 }
 
 @Daily(day = 5)
 class ShedNig extends Shed{
-    public void Block(@NotNull DataContainer lost){
-        System.out.println("Ride the " + lost.getAdress());
+    @Timtim(hour=19, prior = 3, descr = "Find way")
+    @Override
+    void xExec(DataContainer dc) {
+        dc.setPasslvl(0);
+        dc.setAge(25);
+    }
+
+    @Timtim(hour=22, prior = 2, descr = "Hard day")
+    @Override
+    void Fin(DataContainer dc) {
+        dc = null;
     }
 }
