2697c8e424445efe79f4e31bba625a13371d209d
ShchekoturovDA <shchekoturovdmitr@mail.ru>
2024-07-05 11:00:30 +0300

    generate Spring Boot

---
+++ Mvc/src/test/java/sber/spring/Mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package sber.spring.Mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
