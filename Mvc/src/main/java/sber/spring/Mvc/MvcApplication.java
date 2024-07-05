583c3757f3bf33d094c232f39df36d6008d6f5f4
ShchekoturovDA <shchekoturovdmitr@mail.ru>
2024-07-05 11:44:14 +0300

    create page

--- Mvc/src/main/java/sber/spring/Mvc/MvcApplication.java
+++ Mvc/src/main/java/sber/spring/Mvc/MvcApplication.java
@@ -2,12 +2,19 @@ package sber.spring.Mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class MvcApplication {
+public class MvcApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvcApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MvcApplication.class);
+	}
 }
